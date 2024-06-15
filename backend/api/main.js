ff1972671837daa711b122e7be7966597ad23298
Shashank Bhake <shashankbhake123@gmail.com>
2024-06-16 01:24:17 +0530

    D5

---
+++ backend/api/main.js
@@ -0,0 +1,51 @@
+// Import necessary modules
+const express = require("express");
+const axios = require("axios");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+const port = 5005;
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+// Use the cors middleware
+app.use(cors());
+
+// Define a route for handling requests
+app.get("/getOpenAIResponse", async (req, res) => {
+    try {
+        // Extract the query from the request
+        const query = req.query.query;
+
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        // Get OpenAI response
+        const result = await model.generateContent(query);
+        const response = await result.response;
+        const text = response.text();
+        // Extract code from the OpenAI response
+        const openaiResponse = text;
+        const codeBlockRegex = /```(\w+)([\s\S]+?)```/; // Regex to match code blocks for any language
+        const match = openaiResponse.match(codeBlockRegex);
+
+
+        // Check if there is a match
+        if (match && match[2]) {
+            // Extracted code
+            const extractedCode = match[2].trim();
+            // Send the extracted code
+            res.send(extractedCode);
+        } else {
+            // If no code is found, send the full response
+            res.send(openaiResponse);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
